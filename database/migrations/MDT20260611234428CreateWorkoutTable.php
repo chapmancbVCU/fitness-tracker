ef4da2566e3d99068e97058516f0efb04955ad98
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-11 19:45:39 -0400

    Initial commit

---
+++ database/migrations/MDT20260611234428CreateWorkoutTable.php
@@ -0,0 +1,31 @@
+<?php
+namespace Database\Migrations;
+use Core\Lib\Database\Schema;
+use Core\Lib\Database\Blueprint;
+use Core\Lib\Database\Migration;
+
+/**
+ * Migration class for the workout table.
+ */
+class MDT20260611234428CreateWorkoutTable extends Migration {
+    /**
+     * Performs a migration for a new table.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create('workout', function (Blueprint $table) {
+            $table->id();
+
+        });
+    }
+
+    /**
+     * Undo a migration task.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists('workout');
+    }
+}
