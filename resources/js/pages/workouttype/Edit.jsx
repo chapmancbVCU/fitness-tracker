c1688739cf7c336a453ee4468fabc2896f61587d
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-15 20:27:52 -0400

    Started form

--- resources/js/pages/workouttype/Edit.jsx
+++ resources/js/pages/workouttype/Edit.jsx
@@ -1,5 +1,6 @@
 import documentTitle from "@chappy/utils/documentTitle";
 import React from "react";
+import Forms from "@chappy/components/Forms";
 function Edit({ param }) {
 
     if(param === 'new') documentTitle("Add a new workout type");
@@ -10,7 +11,23 @@ function Edit({ param }) {
                 <h1 className="text-center">Add a new workout type</h1>
             ) : (
                 <h1 className="text-center">Edit your workout</h1>
-            )}        
+            )}
+
+            <div className="row align-items-center justify-content-center mt-5">
+                <div className="col-md-6">
+                    <form method="post" action="">
+                        <Forms.CSRFInput />
+                        <Forms.Input 
+                            type="text"
+                            label="Workout Name"
+                            name="name"
+                            value=""
+                            inputAttrs={{className: 'form-control input-sm'}}
+                            outputAttrs={{className: 'form-group mb-3'}}
+                        />
+                    </form>  
+                </div>
+            </div>
         </>
     );
 }        
