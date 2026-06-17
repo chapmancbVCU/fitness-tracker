d2fa15c0c2f267a9a2643fbdd57ea160f8f636c8
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-16 21:12:03 -0400

    Set title and header then pass name to form when editing

--- resources/js/pages/workouttype/Edit.jsx
+++ resources/js/pages/workouttype/Edit.jsx
@@ -2,18 +2,15 @@ import documentTitle from "@chappy/utils/documentTitle";
 import React from "react";
 import Forms from "@chappy/components/Forms";
 import route from "@chappy/utils/route";
-function Edit({ param, errors }) {
 
-    if(param === 'new') documentTitle("Add a new workout type");
+function Edit({ param, errors, workoutType }) {
 
+    const title = (param === 'new') ? "Add a new workout type" : "Edit workout name"
+    documentTitle(title)
     return (
         <>
-            {param === 'new' ? (
-                <h1 className="text-center">Add a new workout type</h1>
-            ) : (
-                <h1 className="text-center">Edit your workout</h1>
-            )}
-
+            <h1 className="text-center">{title}</h1>
+          
             <div className="row align-items-center justify-content-center mt-5">
                 <div className="col-md-6 bg-light p-3">
                     <form className="form" method="post" action="">
@@ -23,7 +20,7 @@ function Edit({ param, errors }) {
                             type="text"
                             label="Workout Name"
                             name="name"
-                            value=""
+                            value={workoutType.name}
                             inputAttrs={{className: 'form-control input-sm'}}
                             outputAttrs={{className: 'form-group mb-3'}}
                         />
