8a02c3f2a9d22786df99e39db93e3f8b3f351468
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-16 21:12:26 -0400

    Added table containing workout types

--- resources/js/pages/workouttype/Index.jsx
+++ resources/js/pages/workouttype/Index.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import documentTitle from "@chappy/utils/documentTitle";
 import route from "@chappy/utils/route";
-function Index() {
-    documentTitle("My Workouts");
 
+function Index({ workoutTypes }) {
+    documentTitle("My Workouts");
     return (
         <>
             <div className="d-flex justify-content-center">
                 <h1 className="text-center">My Workouts</h1>
                 <a href={route('workoutType.edit', ['new'])} className="btn btn-primary btn-sm mx-2 mb-3">
-                    <i class="fa fa-add mt-2"></i>Add
+                    <i className="fa fa-add mt-2"></i>Add
                 </a>
+
             </div>
+            <table className="w-50 mx-auto table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {workoutTypes.map((workoutType) => (
+                        <tr key={workoutType.id}>
+                            <td>
+                                <a href={route('workoutType.Edit', [workoutType.id])}>{workoutType.name}</a> 
+                            </td>
+                            <td></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
             
         </>
     );
