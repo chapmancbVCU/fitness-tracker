9def47b28bfabf199710c2ac52c554bf3d7cb134
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-18 21:13:49 -0400

    Added button to advance to workout setup

--- resources/js/pages/fitness/Index.jsx
+++ resources/js/pages/fitness/Index.jsx
@@ -1,9 +1,22 @@
+import route from "@chappy/utils/route";
 import React from "react";
-function Index() {
+
+function Index({ user }) {
 
     return (
         <>
-            Hello
+            {user ? (
+                <div className="d-flex justify-content-center">
+                    <div className="d-flex align-items-center">
+                        <h1>{user.fname}'s Workout History</h1>
+                        <a href={route('workout.index')} className="btn btn-primary btn-sm mx-2">
+                            <i className="fa fa-add"></i>Begin
+                        </a>
+                    </div>
+                </div>
+            ) : (
+                <h1 className="text-center">Login to view and track your workouts</h1>
+            )}
         </>
     );
 }        
