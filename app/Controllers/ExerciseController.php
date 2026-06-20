e8a02644359d80fefd219c68a5be85ad96a5b799
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-20 17:09:02 -0400

    Performed the following: * Added exercises as prop for indexAction * Added isPost logic for editAction

--- app/Controllers/ExerciseController.php
+++ app/Controllers/ExerciseController.php
@@ -1,6 +1,9 @@
 <?php
 namespace App\Controllers;
+
+use App\Models\Exercise;
 use Core\Controller;
+use Core\Services\AuthService;
 
 /**
  * Undocumented class
@@ -16,10 +19,27 @@ class ExerciseController extends Controller {
     }
 
     public function editAction(mixed $param): void {
+        $user_id = AuthService::currentUser()->id;
+        $exercise = ($param == 'new') ? new Exercise() : Exercise::findById($param);
 
+        if($this->request->isPost()) {
+            $this->request->csrfCheck();
+            $exercise->assign($this->request->get());
+            $exercise->user_id = $user_id;
+            $exercise->save();
+            if($exercise->validationPassed()) redirect('exercise.Index');
+        }
+        $props = [
+            'param' => $param,
+            'errors' => $exercise->getErrorMessages(),
+            'exercise' => $exercise
+        ];
+
+        $this->view->renderJsx('exercise.Edit', $props);
     }
-    
+
     public function indexAction(): void {
-        $this->view->renderJsx('exercise.Index');
+        $exercises = Exercise::findAllByUserId(AuthService::currentUser()->id);
+        $this->view->renderJsx('exercise.Index', ['exercises' => $exercises]);
     }
 }
