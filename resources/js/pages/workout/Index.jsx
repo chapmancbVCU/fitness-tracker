3e533ad0e0878949ec9045abdd01c6c61e9b6a52
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-20 15:50:20 -0400

    Added condition for message if no workout types have been created

--- resources/js/pages/workout/Index.jsx
+++ resources/js/pages/workout/Index.jsx
@@ -1,30 +1,37 @@
 import Forms from "@chappy/components/Forms";
 import route from "@chappy/utils/route";
 import React from "react";
-function Index({ workoutTypes, beginTime }) {
+function Index({ beginTime, workoutTypes }) {
+    
     return (
         <>
             <h1 className="text-center">Workout Setup ({beginTime})</h1>
-            <div className="row align-items-center justify-content-center mt-5">
-                <div className="col-md-6 bg-light p-3">
-                    <form className="form" method="post" action={route('workout.exercises')}>
-                        <Forms.CSRFInput />
-                        <Forms.Select 
-                            label="Select Workout Type"
-                            name="workout_type_id"
-                            value=""
-                            fieldName="name"
-                            options={workoutTypes}
-                            inputAttrs={{className: 'form-select input-sm'}}
-                            divAttrs={{className: 'form-group mb-3'}}
-                        />
-                        <div className="col-md-12 text-end mt-3">
-                            <a href={route('workout')} className="btn btn-default">Cancel</a>
-                            <Forms.SubmitTag label={"Submit"} inputAttrs={{className: 'btn btn-primary'}}/>
-                        </div>
-                    </form>
+            {workoutTypes.length > 0 ? (
+                <div className="row align-items-center justify-content-center mt-5">
+                    <div className="col-md-6 bg-light p-3">
+                        <form className="form" method="post" action={route('workout.exercises')}>
+                            <Forms.CSRFInput />
+                            <Forms.Select 
+                                label="Select Workout Type"
+                                name="workout_type_id"
+                                value=""
+                                fieldName="name"
+                                options={workoutTypes}
+                                inputAttrs={{className: 'form-select input-sm'}}
+                                divAttrs={{className: 'form-group mb-3'}}
+                            />
+                            <div className="col-md-12 text-end mt-3">
+                                <a href={route('workout')} className="btn btn-default">Cancel</a>
+                                <Forms.SubmitTag label={"Submit"} inputAttrs={{className: 'btn btn-primary'}}/>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
+            ) : (
+                <p className="text-center mt-5">You have no workout types yet.  
+                    Click <a href={route('workoutType.edit', ['new'])}>here </a> to add a workout type.
+                </p>
+            )}
         </>
     );
 }        
