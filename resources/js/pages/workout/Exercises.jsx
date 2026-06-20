4b2b917fd301a8f271e0048ebfef74819335c300
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-19 22:05:34 -0400

    Initial commit

---
+++ resources/js/pages/workout/Exercises.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Exercises({ workoutType }) {
+
+    return (
+        <>
+            <h1>{workoutType.name} Day Setup</h1>
+        </>
+    );
+}        
+export default Exercises;
