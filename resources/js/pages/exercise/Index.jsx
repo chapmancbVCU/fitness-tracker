b1038c26a65efd8f16922a88db4e5e3db9bc9c56
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-20 18:10:23 -0400

    Added table to list exercises and add and delete buttons

--- resources/js/pages/exercise/Index.jsx
+++ resources/js/pages/exercise/Index.jsx
@@ -1,7 +1,18 @@
+import documentTitle from "@chappy/utils/documentTitle";
 import route from "@chappy/utils/route";
 import React from "react";
+import Forms from "@chappy/components/Forms";
+
 function Index({ exercises }) {
+    documentTitle("Your Exercises")
     console.log(exercises)
+    async function onDeleteClick(e) {
+        if(!window.confirm("Are you sure")) {
+            e.preventDefault();
+            return false;
+        }
+    }
+
     return (
         <>
             <div className="d-flex justify-content-center align-items-center">
@@ -10,6 +21,36 @@ function Index({ exercises }) {
                     <i className="fa fa-add"></i>Add
                 </a>
             </div>
+            <table className="w-50 mt-5 mx-auto table table-striped table-bordered table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {exercises.map((exercise) => (
+                        <tr key={exercise.id}>
+                            <td>{exercise.name}</td>
+                            <td className="text-center w-25">
+                                <a href={route('exercise.Edit', [exercise.id])} className="btn btn-info btn-sm">
+                                    <i className="fa fa-edit"></i>Edit
+                                </a>
+                                <form method="post"
+                                    action={route('exercise.Delete', [exercise.id])}
+                                    className="d-inline-block"
+                                    onSubmit={onDeleteClick}>
+                                    <Forms.CSRFInput />
+                                    <button type="submit"
+                                        className="btn btn-danger btn-sm ms-2">
+                                        <i className="fa fa-trash"></i> Delete
+                                    </button>
+                                </form>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </>
     );
 }        
