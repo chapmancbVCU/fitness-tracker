5927f939e9ad811af056e54470fa2cb34111f2ee
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-20 17:08:10 -0400

    Added form for adding/editing an exercise

--- resources/js/pages/exercise/Edit.jsx
+++ resources/js/pages/exercise/Edit.jsx
@@ -1,9 +1,35 @@
+import Forms from "@chappy/components/Forms";
+import documentTitle from "@chappy/utils/documentTitle";
 import React from "react";
-function Edit() {
+import route from "@chappy/utils/route";
+function Edit({ param, errors, exercise }) {
+
+    const title = (param === 'new') ? "Add an Exercise" : "Edit Exercise";
+    documentTitle(title)
 
     return (
         <>
-        
+            <h1 className="text-center">{title}</h1>
+            <div className="row align-items-center justify-content-center mt-5">
+                <div className="col-md-6 bg-light p-3">
+                    <form className="form" method="post" action="">
+                        <Forms.CSRFInput />
+                        <Forms.DisplayErrors errors={errors} />
+                        <Forms.Input 
+                            type="text"
+                            label="Exercise Name"
+                            name="name"
+                            value={exercise.name}
+                            inputAttrs={{className: 'form-control input-sm'}}
+                            divAttrs={{className: 'form-group mb-3'}}
+                        />
+                        <div className="col-md-12 text-end mt-3">
+                            <a href={route('exercise')} className="btn btn-default">Cancel</a>
+                            <Forms.SubmitTag label={"Submit"} inputAttrs={{className: 'btn btn-primary'}}/>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </>
     );
 }        
