b4337507df6c5d1a20561b6fade95213007d12ac
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-23 20:24:20 -0400

    Initial commit

---
+++ app/Models/Workout.php
@@ -0,0 +1,51 @@
+<?php
+namespace App\Models;
+use Core\Model;
+
+/**
+ * Implements features of the Workout class.
+ */
+class Workout extends Model {
+
+    // Fields you don't want saved on form submit
+    // public const blackList = [];
+
+    // Set to name of database table.
+    protected static $_table = 'workout';
+
+    // Soft delete
+    // protected static $_softDelete = true;
+    
+    // Fields from your database
+    public $created_at;
+    public $id;
+    public $notes;
+    public $updatedAt;
+    public $userId;
+
+
+    public function afterDelete(): void {
+        // Implement your function
+    }
+
+    public function afterSave(): void {
+        // Implement your function
+    }
+
+    public function beforeDelete(): void {
+        // Implement your function
+    }
+
+    public function beforeSave(): void {
+        // Implement your function
+    }
+
+    /**
+     * Performs validation for the Workout model.
+     *
+     * @return void
+     */
+    public function validator(): void {
+        // Implement your function
+    }
+}
