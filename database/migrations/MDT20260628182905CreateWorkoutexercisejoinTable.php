9df5895d4ddc197c31e73cec2f6b81a77406326f
Chad Chapman <chad.chapman2010@gmail.com>
2026-06-28 14:36:28 -0400

    Initial commit

---
+++ database/migrations/MDT20260628182905CreateWorkoutexercisejoinTable.php
@@ -0,0 +1,34 @@
+<?php
+namespace Database\Migrations;
+use Core\Lib\Database\Schema;
+use Core\Lib\Database\Blueprint;
+use Core\Lib\Database\Migration;
+
+/**
+ * Migration class for the workout-exercise-join table.
+ */
+class MDT20260628182905CreateWorkoutexercisejoinTable extends Migration {
+    /**
+     * Performs a migration for a new table.
+     *
+     * @return void
+     */
+    public function up(): void {
+        Schema::create('workout-exercise-join', function (Blueprint $table) {
+            $table->id();
+            $table->integer('workout_id');
+            $table->index('workout_id');
+            $table->integer('exercise_id')->unique();
+            $table->index('exercise_id');
+        });
+    }
+
+    /**
+     * Undo a migration task.
+     *
+     * @return void
+     */
+    public function down(): void {
+        Schema::dropIfExists('workout-exercise-join');
+    }
+}
